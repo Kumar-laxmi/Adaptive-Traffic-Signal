afea5407134db0638233b6149f1d501939664130
Kumar-laxmi <laxmikantk2002@gmail.com>
2023-09-26 16:26:26 +0530

    Image upload enabled

--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+def index_temp(request):
+    return render(request, 'index_temp.html', {})
--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,6 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('temp/', index_temp),
     path('', index)
 ]
