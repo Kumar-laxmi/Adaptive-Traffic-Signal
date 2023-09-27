cd15e30e55125e639d229bb85ddacd84888fe8d4
Kumar-laxmi <laxmikantk2002@gmail.com>
2023-09-27 10:22:17 +0530

    Image deletion from Database achieved

--- app/custom_storage.py
+++
@@ -1,8 +0,0 @@
-from django.core.files.storage import Storage
-
-class NoStorage(Storage):
-    def _open(self, name, mode='rb'):
-        raise NotImplementedError("Storage backend is disabled")
-
-    def _save(self, name, content):
-        raise NotImplementedError("Storage backend is disabled")
--- app/models.py
+++ app/models.py
@@ -1,11 +1,8 @@
 from django.db import models  
-from django.db.models.signals import post_delete
-from .utils import file_cleanup
-  
-class UploadedImage(models.Model):
-    image = models.ImageField(upload_to='uploads/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+import os
+from django_cleanup import cleanup
 
-post_delete.connect(
-    file_cleanup, sender=UploadedImage, dispatch_uid="gallery.image.file_cleanup"
-)
+@cleanup.select
+class UploadedImage(models.Model):
+    image = models.ImageField(upload_to='uploads/', blank = True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
