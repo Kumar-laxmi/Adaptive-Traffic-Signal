ccf78c7e12ecb8c394b1a82ab3ccb36257ebc758
Kumar-laxmi <laxmikantk2002@gmail.com>
2023-09-27 08:30:17 +0530

    Working on Post Delete

--- app/models.py
+++ app/models.py
@@ -1,5 +1,11 @@
 from django.db import models  
+from django.db.models.signals import post_delete
+from .utils import file_cleanup
   
 class UploadedImage(models.Model):
     image = models.ImageField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+post_delete.connect(
+    file_cleanup, sender=UploadedImage, dispatch_uid="gallery.image.file_cleanup"
+)
