dfac6fe57e7294c6bd4b2dcd61cedefcd92f9be2
Kumar-laxmi <laxmikantk2002@gmail.com>
2023-11-26 09:54:02 +0530

    Simulation and YOLO model added

---
+++ simulation/runner.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.dev/sumo
+# Copyright (C) 2009-2023 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License 2.0 are satisfied: GNU General Public License, version 2
+# or later which is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+
+# @file    runner.py
+# @author  Lena Kalleske
+# @author  Daniel Krajzewicz
+# @author  Michael Behrisch
+# @author  Jakob Erdmann
+# @date    2009-03-26
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import sys
+import optparse
+import random
+
+# we need to import python modules from the $SUMO_HOME/tools directory
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+from sumolib import checkBinary  # noqa
+import traci  # noqa
+
+"""
+def run():
+    step = 0
+    # we start with phase 2 where EW has green
+    traci.trafficlight.setPhase("center", 2)
+    while traci.simulation.getMinExpectedNumber() > 0:
+        traci.simulationStep()
+        if traci.trafficlight.getPhase("center") == 2:
+            # we are not already switching
+            if traci.inductionloop.getLastStepVehicleNumber("0") > 0:
+                # there is a vehicle from the north, switch
+                traci.trafficlight.setPhase("center", 3)
+            else:
+                # otherwise try to keep green for EW
+                traci.trafficlight.setPhase("center", 2)
+        step += 1
+    traci.close()
+    sys.stdout.flush()
+"""
+def run():
+    step = 0
+    timer = 1000                                           #10 seconds timer
+    traci.trafficlight.setPhase("center", 0)
+    while traci.simulation.getMinExpectedNumber() > 0:
+        traci.simulationStep()
+        phase = (step // timer) % 8
+        traci.trafficlight.setPhase("center", phase)
+        step += 1
+
+    traci.close()
+    sys.stdout.flush()
+
+def get_options():
+    optParser = optparse.OptionParser()
+    optParser.add_option("--nogui", action="store_true",
+                         default=False, help="run the commandline version of sumo")
+    options, args = optParser.parse_args()
+    return options
+
+
+# this is the main entry point of this script
+if __name__ == "__main__":
+    options = get_options()
+
+    # this script has been called from the command line. It will start sumo as a
+    # server, then connect and run
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else:
+        sumoBinary = checkBinary('sumo-gui')
+
+
+    # this is the normal way of using traci. sumo is started as a
+    # subprocess and then the python script connects and runs
+    traci.start([sumoBinary, "-c", "data/intersection1.sumocfg",
+                             "--tripinfo-output", "tripinfo.xml"])
+    run()
